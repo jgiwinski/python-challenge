376dbf3f55e02b5b86d791aed9fd0e48e2e3e3c4
jgiwinski <juliaiwinski@gmail.comgit config --global user.name jgiwinskigit add -Apwdgit config user.email juliaiwinski@gmail.com>
2019-09-13 18:11:58 -0700

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,12 +1,12 @@
 # import all the things! 
 import os
-import csv 
+import csv
 
-# declare all the columns
+# Declaring CSV Columns
 date = []
 profLoss = []
 
-# declare all the variables
+# Declaring Variables
 totalMonths = 0
 netTotal = 0
 avgChange = 0
@@ -15,19 +15,16 @@ greatestIncreaseTotal = 0
 greatestDecreaseDate = ''
 greatestDecreaseTotal = 0
 lineBreak = "-"*30
+# Path to collect data from the resources folder
+bankCsv = os.path.join("budget_data.csv")
 
-# path to collect all the things 
-bankcsv = os.path.join('budget_data.csv') 
-
-# create all the functions
+# # Creating a function
 def mean(numbers):
 	return float(sum(numbers)) / max(len(numbers), 1)
-
-# read csv file 
-with open(bankcsv, newline='') as csvfile:
+# Read in the CSV file
+with open(bankCsv, newline="") as csvfile:
 	reader = csv.DictReader(csvfile)
-
-    # Iterating through each row
+	# Iterating through each row
 	for row in reader:
 		# Tracking Date information
 		date.append(row['Date'])
@@ -36,36 +33,39 @@ with open(bankcsv, newline='') as csvfile:
 	
 		# Declaring variable to compare for greatest increase/decrease
 		value = int(row['Profit/Losses'])
-		# greatest increase 
+		# Greatest Increase conditional
 		if greatestIncreaseTotal < value:
 			greatestIncreaseTotal = value
 			greatestIncreaseDate = row['Date']
-
-		# greatest decrease 
+		# Greatest Decrease conditional
 		if greatestDecreaseTotal > value:
 			greatestDecreaseTotal = value
 			greatestDecreaseDate = row['Date']
 		
-		# total months
+		# Tracking total months
 		totalMonths = totalMonths + 1
-		# total of profits/losses
+		# Tracking total of profits/losses
 		netTotal = netTotal + int(row['Profit/Losses'])
-        
-# average change
+# Getting the average change
 avgChange = mean(profLoss)
-
-# formatting average change, greatest increase/decrease, and net total
+# Formatting Average Change, greatest increase/decrease, and net total
 netTotal = "${:0,.2f}".format(netTotal)
 avgChange = "${:0,.2f}".format(avgChange)
 greatestIncreaseTotal = "${:0,.2f}".format(greatestIncreaseTotal)
 greatestDecreaseTotal = "${:0,.2f}".format(greatestDecreaseTotal).replace('$-','-$')
-
 # Stores all needed values in a variable for moving to a text file
-txtFileInfo = f"Financial Analysis"'\n'f"{lineBreak}"'\n'f"Total months: {totalMonths}"'\n'f"Total: {netTotal}"'\n'f"Average Change: {avgChange}"'\n'f"Greatest increase in Profits: {greatestIncreaseDate} {greatestIncreaseTotal}"'\n'f"Greatest decrease in Profits: {greatestDecreaseDate} {greatestDecreaseTotal}"
+nl = "\n"
+txtFileInfo = f"Financial Analysis {nl}" + \
+	f"{lineBreak}\n" + \
+	f"Total months: {totalMonths}\n" + \
+	f"Total: {netTotal}\n" + \
+	f"Average Change: {avgChange}\n" + \
+	f"Greatest increase in Profits: {greatestIncreaseDate} {greatestIncreaseTotal}\n" + \
+	f"Greatest decrease in Profits: {greatestDecreaseDate} {greatestDecreaseTotal}"
 
 # Prints requested variables to console
 print(txtFileInfo)
-
+# Creates and writes variables to a text file
 textFile = open("bankFile.txt","w")
 with open("bankFile.txt", "w") as att_file:
 	att_file.write(txtFileInfo)
